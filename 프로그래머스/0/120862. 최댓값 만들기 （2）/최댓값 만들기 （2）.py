e8ed7e802885b7eef06cff67fd3a7e5bcd65f2b4
LeeSeungjun <60598355+junslee96@users.noreply.github.com>
2025-01-10 07:02:44 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    numbers.sort()
+    plus_max=numbers[-1]*numbers[-2]
+    minus_max=numbers[0]*numbers[1]
+    if plus_max>minus_max:
+        return plus_max
+    else:
+        return minus_max

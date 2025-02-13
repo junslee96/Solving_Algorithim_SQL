89351b280d4ee226308cffb95e3f4d8588174ac3
LeeSeungjun <60598355+junslee96@users.noreply.github.com>
2025-02-14 00:26:51 +0900

    [level 0] Title: 코드 처리하기, Time: 15.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,19 @@
+def solution(code):
+    mode = 0
+    ret=""
+    
+    for idx in range(len(code)):
+        if mode == 0:
+            if code[idx] != "1":
+                if idx % 2 == 0:
+                    ret += code[idx]
+            else:
+                mode = 1
+        elif mode == 1:
+            if code[idx] != "1":
+                if idx % 2 == 1:
+                    ret += code[idx]
+            else:
+                mode = 0       
+    return ret if ret else "EMPTY"
+

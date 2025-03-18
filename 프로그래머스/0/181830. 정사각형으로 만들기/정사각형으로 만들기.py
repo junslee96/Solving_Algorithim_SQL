bd9db951f984a1d28acf3bc374df06f2eed3f517
LeeSeungjun <60598355+junslee96@users.noreply.github.com>
2025-03-18 15:29:31 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.11 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    rows = len(arr)
+    cols = len(arr[0])
+
+    if rows > cols:
+        for row in arr:
+            row.extend([0] * (rows - cols))
+    elif cols > rows:
+        arr.extend([[0] * cols for _ in range(cols - rows)])
+
+    return arr

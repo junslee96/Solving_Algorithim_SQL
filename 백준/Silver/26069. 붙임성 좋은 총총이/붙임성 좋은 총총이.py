95eac1dd0a99b2d31581e9a44dfcca15b967a170
LeeSeungjun <60598355+junslee96@users.noreply.github.com>
2025-10-10 15:22:03 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+dance = {'ChongChong'}
+for _ in range(N):
+    a,b = input().split()
+    if a in dance or b in dance:
+        dance.add(a)
+        dance.add(b)
+print(len(dance))     
